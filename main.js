d91bd832f4f8a55f32124d0e14baebcd2f218737
Neil Sullivan <ac30delay@gmail.com>
2022-12-12 10:05:45 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const originalDisorderFormat =
+  'Depression|$|Bipolar|$|Manic|$|Anxiety|$|Anorexia|$|Posttraumtic Stress|$|Seasonal Affective|$|Bulimia'
+console.log(originalDisorderFormat)
+  indDisorderFormat = originalDisorderFormat.split ('|$|')
+  
+console.log("first\n", indDisorderFormat)
+
